d49c9abf5312fe21f04b076c2ad1202455eb575f
foxi-yiyi <foxiyiyi12081003@gmail.com>
2021-10-28 09:23:24 +0800

    update winback script

--- win_backup/backup.bat
+++ win_backup/backup.bat
@@ -1,5 +1,5 @@
 set source_dir=D:\forbid
-set target_dir=I:\Documents
+set target_dir=%~dp0
 
-robocopy %source_dir% %target_dir% /s /MIR /R:3 /W:60 /LOG:%target_dir%\copyLog.txt /xf *.bat *.lnk *.ini
+robocopy %source_dir% %target_dir% *.* /E /COPY:DAT /MT:8 /s /MIR /R:3 /W:60  /xf *.bat *.lnk *.ini *.ezy  #/LOG:%target_dir%\copyLog.txt
 pause
