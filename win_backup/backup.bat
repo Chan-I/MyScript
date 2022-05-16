c580c8248e249811778cc91295b69a66303664f3
Chan-i Chiang <41983967+Chan-I@users.noreply.github.com>
2022-05-16 09:43:38 +0800

    更新增量同步脚本
    原先的脚本会保持源/目标目录永远保持镜像关系，而现在仅做增量同步，不再删除目标目录中的源目录不存在的文件。

--- win_backup/backup.bat
+++ win_backup/backup.bat
@@ -1,5 +1,5 @@
 set source_dir=D:\forbid
 set target_dir=%~dp0
 
-robocopy %source_dir% %target_dir% *.* /E /COPY:DAT /MT:8 /s /MIR /R:3 /W:60  /xf *.bat *.lnk *.ini *.ezy  #/LOG:%target_dir%\copyLog.txt
+robocopy %source_dir% %target_dir% *.* /E /COPY:DAT /MT:8 /s /R:3 /W:60  /xf *.bat *.lnk *.ini *.ezy  #/LOG:%target_dir%\copyLog.txt
 pause
