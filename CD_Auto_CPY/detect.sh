e14bcbbb517fedc194ba50a5c902b1401cfcbb18
Chan-i Chiang <41983967+Chan-I@users.noreply.github.com>
2022-11-04 18:58:39 +0800

    Update detect.sh

--- CD_Auto_CPY/detect.sh
+++ CD_Auto_CPY/detect.sh
@@ -7,26 +7,33 @@ do
     cdrom=`blkid | grep udf`
     #cdrom=`blkid | grep xfs`
     if [ "$?" -eq 0 ]
-    then
-       dev_path=`echo $cdrom | awk '{print $1}' | sed 's/\:$//g'`
-       path=`mount | grep $dev_path`
-       path1=${path% *}
-       path2=${path1% *}
-       path3=${path2% *}
-       path4=${path3#* }
-       mount_path=${path4#* }
-
-       echo $dev_path 
-       echo $mount_path 
-       bingo=`stat $dest_path &>/dev/null`
-       if [ "$?" -ne 0 ]
-       then
-           mkdir -p $dest_path
-           cp -r "$mount_path" $dest_path
-           #echo "cp -rf "$mount_path" $dest_path"
-       else
-           echo "Bingo!!!!"
-       fi
+        then
+        dev_path=`echo $cdrom | awk '{print $1}' | sed 's/\:$//g'`
+        path=`mount | grep $dev_path`
+        if test -z "$path"; 
+            then
+            sleep 2
+        else
+            echo "found! $path"
+            path1=${path% *}
+            path2=${path1% *}
+            path3=${path2% *}
+            path4=${path3#* }
+            mount_path=${path4#* }
+            echo $dev_path 
+            echo $mount_path 
+            bingo=`stat $dest_path &>/dev/null`
+            if [ "$?" -ne 0 ]
+                then
+                mkdir -p $dest_path
+                cp -r "$mount_path" $dest_path
+                sleep 20
+                break
+                #echo "cp -rf "$mount_path" $dest_path"
+            else
+                echo "Bingo!!!!"
+            fi
+        fi
     fi
     sleep 2
 done
